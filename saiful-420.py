a19332bdad7080e0df2cf849c6826a135e2f1b94
SAIFUL-HACKER420 <117816561+SAIFUL-HACKER420@users.noreply.github.com>
2022-11-14 20:36:24 +0600

    Create saiful-420.py

---
+++ saiful-420.py
@@ -0,0 +1,201 @@
+}[
+
+    {
+
+        "key": "dbln",
+
+        "value": "%7B%22100086521044063%22%3A%22blblmY87%22%7D",
+
+        "domain": "facebook.com",
+
+        "path": "/login/device-based/",
+
+        "hostOnly": false,
+
+        "creation": "2022-11-14T12:52:08.150Z",
+
+        "lastAccessed": "2022-11-14T12:52:08.151Z"
+
+    },
+
+    {
+
+        "key": "datr",
+
+        "value": "ZppxYwS_SJ8qqDR7odMxOeys",
+
+        "domain": "facebook.com",
+
+        "path": "/",
+
+        "hostOnly": false,
+
+        "creation": "2022-11-14T12:52:08.151Z",
+
+        "lastAccessed": "2022-11-14T12:52:08.151Z"
+
+    },
+
+    {
+
+        "key": "sb",
+
+        "value": "ZppxY6vARQ15awShO63gIZN2",
+
+        "domain": "facebook.com",
+
+        "path": "/",
+
+        "hostOnly": false,
+
+        "creation": "2022-11-14T12:52:08.151Z",
+
+        "lastAccessed": "2022-11-14T12:52:08.151Z"
+
+    },
+
+    {
+
+        "key": "zsh",
+
+        "value": "ASRn5JzpdgL8KEkx6Fvd8XYLLNeGRlUhYTT6_nQw_oyFZT0x68vuY5DXGUtqqd6cV69yFGweF8_XJgT8bEcSmKxxQblbh33TAQgJ1lH0pk7ULoQ6AZzhCC6ZM3yDgWEUIuKhQPdV0dujvM_rl--QiG2TqBmubxi3eBEdCc8giw4voGshN9HJvAo3zO0uA3R8hKANQ5CLQvCcwdR30Q2wrvd_4867vZK8spcAd3NUu4cmlNKtWgyViiVidqIuB9FiLlmo7ki2FHQgIJIuKZUrVQruZReDU6zxRHSR_3Ng9iLmLuqq47TU95UPbqBYDQ6SlQuXWu0lmKBbko9iZutAXm8",
+
+        "domain": "facebook.com",
+
+        "path": "/",
+
+        "hostOnly": false,
+
+        "creation": "2022-11-14T12:52:08.151Z",
+
+        "lastAccessed": "2022-11-14T12:52:08.151Z"
+
+    },
+
+    {
+
+        "key": "locale",
+
+        "value": "en_US",
+
+        "domain": "facebook.com",
+
+        "path": "/",
+
+        "hostOnly": false,
+
+        "creation": "2022-11-14T12:52:08.151Z",
+
+        "lastAccessed": "2022-11-14T12:52:08.151Z"
+
+    },
+
+    {
+
+        "key": "fr",
+
+        "value": "0Tbj9WDRhdf2xje6K.AWXmO5LRp152noLnhXt8nbTnBPs.BjcZpm.in.AAA.0.0.Bjchuw.AWWV-QmjBLA",
+
+        "domain": "facebook.com",
+
+        "path": "/",
+
+        "hostOnly": false,
+
+        "creation": "2022-11-14T12:52:08.151Z",
+
+        "lastAccessed": "2022-11-14T12:52:08.151Z"
+
+    },
+
+    {
+
+        "key": "c_user",
+
+        "value": "100086521044063",
+
+        "domain": "facebook.com",
+
+        "path": "/",
+
+        "hostOnly": false,
+
+        "creation": "2022-11-14T12:52:08.151Z",
+
+        "lastAccessed": "2022-11-14T12:52:08.151Z"
+
+    },
+
+    {
+
+        "key": "xs",
+
+        "value": "35%3ADHYEVqmBeWe_gg%3A2%3A1668422576%3A-1%3A11716",
+
+        "domain": "facebook.com",
+
+        "path": "/",
+
+        "hostOnly": false,
+
+        "creation": "2022-11-14T12:52:08.151Z",
+
+        "lastAccessed": "2022-11-14T12:52:08.151Z"
+
+    },
+
+    {
+
+        "key": "m_page_voice",
+
+        "value": "100086521044063",
+
+        "domain": "facebook.com",
+
+        "path": "/",
+
+        "hostOnly": false,
+
+        "creation": "2022-11-14T12:52:08.151Z",
+
+        "lastAccessed": "2022-11-14T12:52:08.151Z"
+
+    },
+
+    {
+
+        "key": "m_pixel_ratio",
+
+        "value": "1.875",
+
+        "domain": "facebook.com",
+
+        "path": "/",
+
+        "hostOnly": false,
+
+        "creation": "2022-11-14T12:52:08.151Z",
+
+        "lastAccessed": "2022-11-14T12:52:08.151Z"
+
+    },
+
+    {
+
+        "key": "wd",
+
+        "value": "384x750",
+
+        "domain": "facebook.com",
+
+        "path": "/",
+
+        "hostOnly": false,
+
+        "creation": "2022-11-14T12:52:08.151Z",
+
+        "lastAccessed": "2022-11-14T12:52:08.151Z"
+
+    }
+
+]
